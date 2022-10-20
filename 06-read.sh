bfcacb9543aaf74bd22b73e06c7b3faab15597da
Swarna Ganesh <swarna.ganesh@lntecc.com>
2022-10-20 17:41:28 +0530

    comit

--- 06-read.sh
+++ 06-read.sh
@@ -4,4 +4,4 @@ read -p 'Enter your name:' NAME
 echo -e "Name of the executed user is $NAME"
 
 read -p 'Enter the valu of of x:' X
-echo -e "Value of y is $X($X+1)"
+echo -e "Value of y is $X($X +1)"
