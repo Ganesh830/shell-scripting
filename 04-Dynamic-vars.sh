6c78f3fd96c156e5cb99185e556634bd0b0f19f2
Swarna Ganesh <swarna.ganesh@lntecc.com>
2022-10-20 17:10:51 +0530

    my commit

--- 04-Dynamic-vars.sh
+++ 04-Dynamic-vars.sh
@@ -2,6 +2,8 @@
 
 TODAY_DATE="$(date +%F)"
 NO_OF_USERS="$(who | wc -l)"
+UP Time="$(uptime)"
 
 echo "Good Morning, Todays date is ${TODAY_DATE}"
 echo "Number of sessions opened are : ${NO_OF_USERS}"
+echo "up time of the server is: ${UP Time}"
