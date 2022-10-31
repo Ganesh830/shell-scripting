72b3a9516c2e16d2a30f7c6ea24c3d2bad4ee8c6
Swarna Ganesh <swarna.ganesh@lntecc.com>
2022-10-31 18:09:20 +0530

    launch instance with SG_ID&Tag

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -3,7 +3,7 @@
 COMPONENT=$1
 
 AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
-SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=b51-allow-all | jd '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+SG_ID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 
 echo "AMI ID used to launch the instance is :$AMI_ID"
 echo "SG ID used to launch the instance is :$SG_ID"
